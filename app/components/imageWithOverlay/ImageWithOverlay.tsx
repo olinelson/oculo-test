746b429cea7cdde7f75094b46c3ec4bfbfcdd8f1
Oli Nelson <oliver.nelson@nirovision.com>
2023-03-28 19:27:00 +1100

    cleanup

--- app/components/imageWithOverlay/ImageWithOverlay.tsx
+++ app/components/imageWithOverlay/ImageWithOverlay.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { ExamImage, ExamImageWithDate } from "../../domain/Exam"
+import { ExamImageWithDate } from "../../domain/Exam"
 import styles from "./imageWithOverlay.module.css"
 
 type Props = {
